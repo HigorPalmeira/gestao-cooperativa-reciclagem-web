59cfbf8e0b26d53824251f820838e343452df0c6
Higor Palmeira <higorpresente05@gmail.com>
2026-02-04 19:38:16 -0300

    Iniciando TipoMaterialService

--- src/main/java/com/gestaocooperativareciclagem/service/TipoMaterialService.java
+++ src/main/java/com/gestaocooperativareciclagem/service/TipoMaterialService.java
@@ -1,5 +1,36 @@
 package com.gestaocooperativareciclagem.service;
 
+import java.util.List;
+
+import com.gestaocooperativareciclagem.dao.TipoMaterialDAO;
+import com.gestaocooperativareciclagem.model.TipoMaterial;
+
 public class TipoMaterialService {
+	
+	private TipoMaterialDAO tipoMaterialDao;
+
+	public TipoMaterialService(TipoMaterialDAO tipoMaterialDao) {
+		this.tipoMaterialDao = tipoMaterialDao;
+	}
+	
+	public void inserirTipoMaterial(String nome, String descricao) {
+		
+		if (nome == null || nome.isBlank()) {
+			throw new RuntimeException("Nome inválido! É necessário informar um nome para criar um novo tipo de material.");
+		}
+		
+		if (descricao == null || descricao.isBlank()) {
+			throw new RuntimeException("Descrição inválida! É necessário informar uma descrição para criar um novo tipo de material.");
+		}
+		
+		TipoMaterial tipoMaterial = new TipoMaterial(nome, descricao);
+		
+		tipoMaterialDao.inserirTipoMaterial(tipoMaterial);
+
+	}
+	
+	public List<TipoMaterial> listarTiposMaterial() {
+		
+	}
 
 }
