fa0690f6e6792fc3a57faf081652e8740d309e2d
Higor Palmeira <higorpresente05@gmail.com>
2026-03-06 22:35:36 -0300

    Criando consulta dinâmica ao banco de dados 6: Venda

--- src/main/java/com/gestaocooperativareciclagem/dao/VendaDAO.java
+++ src/main/java/com/gestaocooperativareciclagem/dao/VendaDAO.java
@@ -246,5 +246,77 @@ public class VendaDAO {
 		}
 		
 	}
+	
+	public List<Venda> listarVendasComParametro(Integer paramIdVenda, Date paramDtVenda, BigDecimal paramValorTotalVenda, String paramCnpjCliente) throws SQLException {
+		
+		List<Venda> listaVendas = new ArrayList<>();
+		
+		List<Object> parametros = new ArrayList<>();
+		String select = buildQuerySelect(parametros, paramIdVenda, paramDtVenda, paramValorTotalVenda, paramCnpjCliente);
+		
+		try(Connection conexao = Conexao.getConnection();
+				PreparedStatement pst = conexao.prepareStatement(select);) {
+			
+			for (int i=0; i<parametros.size(); i++) {
+				pst.setObject(i+1, parametros.get(i));
+			}
+			
+			try(ResultSet rset = pst.executeQuery();) {
+				
+				while(rset.next()) {
+					
+					int idVenda = rset.getInt("id_venda");
+					Date dtVenda = rset.getDate("dtVenda_venda");
+					BigDecimal valorTotalVenda = rset.getBigDecimal("valor_total_venda");
+					
+					String cnpjCliente = rset.getString("cnpj_cliente");
+					String nomeEmpresa = rset.getString("nome_empresa_cliente");
+					String contatoPrincipal = rset.getString("contato_principal_cliente");
+					String emailCliente = rset.getString("email_contato_cliente");
+					Date dtCadastroCliente = rset.getDate("dtCadastro_cliente");
+					
+					Cliente cliente = new Cliente(cnpjCliente, nomeEmpresa, contatoPrincipal, emailCliente, dtCadastroCliente);
+					
+					listaVendas.add(new Venda(idVenda, dtVenda, valorTotalVenda, cliente));
+					
+				}
+				
+			}
+			
+		}
+		
+		return listaVendas;
+		
+	}
+	
+	private String buildQuerySelect(List<Object> parametros, Integer idVenda, Date dtVenda, BigDecimal valorTotalVenda, String cnpjCliente) {
+		
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("select * from info_venda where 1=1");
+		
+		if (idVenda != null && idVenda != 0) {
+			builder.append(" and id_venda = ?");
+			parametros.add(idVenda);
+		}
+		
+		if (dtVenda != null) {
+			builder.append(" and dtVenda_venda = ?");
+			parametros.add(dtVenda);
+		}
+		
+		if (valorTotalVenda != null) {
+			builder.append(" and valor_total_venda = ?");
+			parametros.add(valorTotalVenda);
+		}
+		
+		if (cnpjCliente != null && !cnpjCliente.isBlank()) {
+			builder.append(" and cnpj_cliente like ?");
+			parametros.add("%" + cnpjCliente.trim() + "%");
+		}
+		
+		return builder.toString();
+		
+	}
 
 }
